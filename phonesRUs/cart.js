f7d1936e7ca23bb5a80a51dd5a6ceca39b45d777
John Pellow <pellowj@aol.com>
2018-05-20 16:50:52 -0500

    Add files via upload

---
+++ phonesRUs/cart.js
@@ -0,0 +1,165 @@
+//android phones
+
+function cartSamsungGalaxyS9() {
+  var samsungGalaxyS9Total = 719.99;
+  localStorage.setItem("samsungGalaxyS9", samsungGalaxyS9Total);
+  if (!Number(localStorage.getItem("samsungGalaxyS9")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartGooglePixel2() {
+  var googlePixel2Total = 699.99;
+  localStorage.setItem("googlePixel2", googlePixel2Total);
+  if (!Number(localStorage.getItem("googlePixel2")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartlgv30() {
+  var lgv30Total = 634.99;
+  localStorage.setItem("lgv30", lgv30Total);
+  if (!Number(localStorage.getItem("lgv30")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartOnePlus5() {
+  var onePlus5Total = 554.99;
+  localStorage.setItem("onePlus5", onePlus5Total);
+  if (!Number(localStorage.getItem("onePlus5")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartGooglePixelXL() {
+  var googlePixelXLTotal = 665.00;
+  localStorage.setItem("googlePixelXL", googlePixelXLTotal);
+  if (!Number(localStorage.getItem("googlePixelXL")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartMotoZ2Force() {
+  var motoZ2ForceTotal = 739.99;
+  localStorage.setItem("motoZ2Force", motoZ2ForceTotal);
+  if (!Number(localStorage.getItem("motoZ2Force")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartsonyxperoaxz2() {
+  var sonyxperoaxz2Total = 796.39;
+  localStorage.setItem("sonyxperoaxz2", sonyxperoaxz2Total);
+  if (!Number(localStorage.getItem("sonyxperoaxz2")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function carthuaweimate10pro() {
+  var huaweimate10proTotal = 679.99;
+  localStorage.setItem("huaweimate10pro", huaweimate10proTotal);
+  if (!Number(localStorage.getItem("huaweimate10pro")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartOnePlus5T() {
+  var onePlus5TTotal = 539.74;
+  localStorage.setItem("onePlus5T", onePlus5TTotal);
+  if (!Number(localStorage.getItem("onePlus5T")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartGooglePixel() {
+  var googlePixelTotal = 289.39;
+  localStorage.setItem("googlePixel", googlePixelTotal);
+  if (!Number(localStorage.getItem("googlePixel")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartSamsungGalaxyS7() {
+  var samsungGalaxyS7Total = 321.95;
+  localStorage.setItem("samsungGalaxyS7", samsungGalaxyS7Total);
+  if (!Number(localStorage.getItem("samsungGalaxyS7")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartMotoZ() {
+  var motoZTotal = 304.95;
+  localStorage.setItem("motoZ", motoZTotal);
+  if (!Number(localStorage.getItem("motoZ")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+// iphones
+
+function cartIPhone8() {
+  var iPhone8Total = 634.99;
+  localStorage.setItem("iPhone8", iPhone8Total);
+  if (!Number(localStorage.getItem("iPhone8")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartIPhone7Plus() {
+  var iPhone7PlusTotal = 499.99;
+  localStorage.setItem("iPhone7Plus", iPhone7PlusTotal);
+  if (!Number(localStorage.getItem("iPhone7Plus")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartIPhone7() {
+  var iPhone7Total = 389.99;
+  localStorage.setItem("iPhone7", iPhone7Total);
+  if (!Number(localStorage.getItem("iPhone7")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartIPhoneSE() {
+  var iPhoneSETotal = 339.99;
+  localStorage.setItem("iPhoneSE", iPhoneSETotal);
+  if (!Number(localStorage.getItem("iPhoneSE")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartIPhone6SPlus() {
+  var iPhone6SPlusTotal = 296.99;
+  localStorage.setItem("iPhone6SPlus", iPhone6SPlusTotal);
+  if (!Number(localStorage.getItem("iPhone6SPlus")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
+
+function cartIPhone6S() {
+  var iPhone6STotal = 223.95;
+  localStorage.setItem("iPhone6S", iPhone6STotal);
+  if (!Number(localStorage.getItem("iPhone6S")) == 0) {
+    alert('Item has been added to cart');
+
+  }
+}
